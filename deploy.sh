24cf6cf0f4e8a8e11699ae1f2cf3efc6b1a80e55
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-12-19 16:46:38 +0100

    Fixed init

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-[ $# -eq 0 -o $1 -gt 5 ] && { echo "Usage: ./deploy.sh num_workers(max 5 workers)"; exit 1; }
-
+if [ $# -eq 0 ] || [ $1 -gt 5 ]
+then
+  echo "Usage: ./deploy.sh num_workers(max 5 workers)"
+  exit 1
+fi
 
 export $(grep -v '^#' .env | xargs)
 docker build -t "$REGISTRY_NAME.azurecr.io"/coordinator --target coordinator .
@@ -9,7 +12,7 @@ docker build -t "$REGISTRY_NAME.azurecr.io"/worker --target worker .
 docker push "$REGISTRY_NAME.azurecr.io"/coordinator
 docker push "$REGISTRY_NAME.azurecr.io"/worker
 
-#az container start -g cbdp-resourcegroup --name coordinator
+az container start -g cbdp-resourcegroup --name coordinator
 COORDINATOR=$(az container show --resource-group cbdp-resourcegroup --name coordinator --query ipAddress.ip --output tsv)
 for (( i=1; i<=$1; i++ ))
 do
